8ed2e81cae10613066639787072c723010c28a44
Parikshit Sangar <85061567+ParikshitSangar2@users.noreply.github.com>
2022-06-10 00:47:35 +0530

    Subsetsumtok.cpp

---
+++ Subsetssumtok.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+#include <vector>
+void help(int input[], int size, int k ,vector<int> ans)
+{
+    if(size==0)
+    {
+        if(k==0)
+        {
+            for(int i: ans)
+                cout<<i<<" ";
+            cout<<endl;
+        } 
+        return;
+    }
+    help(input+1,size-1,k,ans);//not including
+    ans.push_back(input[0]);
+    help(input+1,size-1,k-input[0],ans);// including                             
+    return;
+}
+void printSubsetSumToK(int input[], int size, int k) {
+    vector<int > ans;  
+    help(input,size,k,ans); 
+}
+int main() {
+  int input[1000],length,k;
+  cin >> length;
+  for(int i=0; i < length; i++)
+    cin >> input[i];
+  cin>>k;
+  printSubsetSumToK(input, length,k);
+}
